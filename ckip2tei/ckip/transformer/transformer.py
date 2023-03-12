4d93cf4a37f16e99b356a5031cae03837e90d216
Retr0327 <philcoke35@gmail.com>
2023-03-12 19:14:36 +0800

    Implement async word segmentation and part-of-speech tagging

---
+++ ckip2tei/ckip/transformer/transformer.py
@@ -0,0 +1,42 @@
+import asyncio
+
+from ..model import CKIPClient
+
+ws_driver, pos_driver = CKIPClient().connect()
+
+
+def remove_empty_strings(sentence_list: list[str]) -> list[str]:
+    """Remove empty strings from a list of sentences."""
+    return [sentence for sentence in sentence_list if sentence != ""]
+
+
+async def pack_ws_pos(ws_pos_pair: tuple) -> list[tuple]:
+    """The pack_ws_pos_sentece method packs a word and its part-of-speech to a pair.
+
+    Args:
+        ws_pos_pair (tuple): the pair of a word and its corresponding part-of-speech
+    Returns:
+        a list of tuples: [
+            ('我', 'Nh'),
+            ('喜歡', 'VK'),
+            ('程式', 'Na')
+        ]
+    """
+    sentence_ws, sentence_pos = ws_pos_pair
+    assert len(sentence_ws) == len(sentence_pos)
+    return list(zip(sentence_ws, sentence_pos))
+
+
+async def transform(sentence_list: list[str]):
+    """The transform method transforms the sentences in a list to word segmentation and
+    part-of-speech results.
+    """
+    is_empty = not sentence_list or all(sentence == "" for sentence in sentence_list)
+
+    if is_empty:
+        return sentence_list
+
+    filtered_list = remove_empty_strings(sentence_list)
+    ws_pipeline = ws_driver(filtered_list, use_delim=True)
+    pos_pipeline = pos_driver(ws_pipeline, use_delim=True)
+    return await asyncio.gather(*map(pack_ws_pos, zip(ws_pipeline, pos_pipeline)))
