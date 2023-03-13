5858a1d31e38f4e4407d546a0dbc77d59f894ed2
Retr0327 <philcoke35@gmail.com>
2023-03-13 17:43:51 +0800

    Refactor create_tags and generate_tei_xml to accept `media` argument

--- ckip2tei/tei/tei.py
+++ ckip2tei/tei/tei.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Literal
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
 
@@ -7,17 +8,19 @@ from .tags.meta import create_meta_tags
 from .utils import get_year
 
 PostData = dict[str, str | dict[str, int] | list[dict[str, str]]]
+Media = Literal["ptt", "dcard"]
 
 
-async def create_tags(root: ET.Element, post_data: PostData) -> None:
+async def create_tags(root: ET.Element, post_data: PostData, media: Media) -> None:
     """The create_tags function creates TEI XML tags and add them to the root.
 
     Args:
         root (xml.etree.ElementTree.Element): the root element of the TEI XML tree
         post_data (PostData): the post data
+        media (Media): a Taiwan social media name
     """
     meta_data = {
-        "media": "ptt",
+        "media": media,
         "author": post_data.get("author"),
         "post_id": post_data.get("post_id"),
         "year": get_year(post_data.get("date")),
@@ -31,14 +34,14 @@ async def create_tags(root: ET.Element, post_data: PostData) -> None:
     await asyncio.gather(task1, task2)
 
 
-def generate_tei_xml(post_data: PostData) -> str:
+def generate_tei_xml(post_data: PostData, media: Media) -> str:
     """The generate_tei_xml function generates TEI XML string.
 
     Args:
         post_data (PostData): the post data
     """
     root = ET.Element("TEI.2")
-    asyncio.run(create_tags(root, post_data))
+    asyncio.run(create_tags(root, post_data, media))
 
     xml_str = minidom.parseString(ET.tostring(root)).childNodes[0]
     return xml_str.toprettyxml(indent="   ")
