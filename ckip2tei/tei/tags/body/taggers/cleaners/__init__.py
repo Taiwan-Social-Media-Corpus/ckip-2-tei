121acd36a785ebe8a53ae87b56eedc736e0fdb3d
Retr0327 <philcoke35@gmail.com>
2023-03-14 14:53:00 +0800

    Export title, body, and comments cleaner classes

--- ckip2tei/tei/tags/body/taggers/cleaners/__init__.py
+++ ckip2tei/tei/tags/body/taggers/cleaners/__init__.py
@@ -1,3 +1,8 @@
 from .base import Cleaner
+from .cleaners import (
+    BodyCleaner,
+    CommentsCleaner,
+    TitleCleaner,
+)
 
-__all__ = ["Cleaner"]
+__all__ = ["Cleaner", "TitleCleaner", "BodyCleaner", "CommentsCleaner"]
