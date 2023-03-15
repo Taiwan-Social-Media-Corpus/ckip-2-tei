112d4ef801fb13fef4f7d42d5b57d8a825bceb72
Retr0327 <philcoke35@gmail.com>
2023-03-15 14:57:17 +0800

    Refactor `transform` to handle both strings and lists of strings

--- ckip2tei/ckip/transformer/transformer.py
+++ ckip2tei/ckip/transformer/transformer.py
@@ -22,15 +22,16 @@ async def pack_ws_pos(ws_pos_pair: tuple) -> list[tuple]:
     return list(zip(sentence_ws, sentence_pos))
 
 
-async def transform(sentence: str):
+async def transform(sentence: str | list[str]):
     """The transform method transforms the sentences in a list to word segmentation and
     part-of-speech results.
     """
-    stripped_sentence = sentence.strip()
+    invalid_sentence = not sentence or all(value == "" for value in sentence)
 
-    if not stripped_sentence:
+    if invalid_sentence:
         return []
 
-    ws_pipeline = ws_driver([stripped_sentence], use_delim=True)
+    data = [sentence.strip()] if isinstance(sentence, str) else sentence
+    ws_pipeline = ws_driver(data, use_delim=True)
     pos_pipeline = pos_driver(ws_pipeline, use_delim=True)
     return await asyncio.gather(*map(pack_ws_pos, zip(ws_pipeline, pos_pipeline)))
