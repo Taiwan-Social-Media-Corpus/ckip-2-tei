aa42a2ce33d678f31e27264ca43fc91998c1b790
Retr0327 <retr0327.dev@gmail.com>
2024-04-13 17:06:40 +0800

    refactor: refactor path and model management with dynamic configuration settings (ref #1)

--- ckip2tei/ckip/model/ckip.py
+++ ckip2tei/ckip/model/ckip.py
@@ -1,32 +1,22 @@
-from dataclasses import (
-    dataclass,
-    field,
-)
+from dataclasses import dataclass
 from pathlib import Path
 import pickle
 
-from ckip2tei.config import (
-    CKIP_DIR,
-    CKIP_PATH,
-    NLP_MODEL,
-)
+from ckip2tei.config import config
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass
 class CKIPClient:
     """
     The CKIPClient object connects to ckip drivers.
     """
-
-    _ckip_path: str = field(init=False, default=CKIP_PATH)
-    _nlp_model: str = field(init=False, default=NLP_MODEL)
 
     def __post_init__(self) -> None:
         self.on_ready()
 
     def on_ready(self) -> None:
         """The on_ready method initializes and caches the CKIP drivers."""
-        has_path = Path(self._ckip_path).exists()
+        has_path = Path(config.config_drivers_path).exists()
 
         if not has_path:
             from ckip_transformers.nlp import (
@@ -34,13 +24,13 @@ class CKIPClient:
                 CkipWordSegmenter,
             )
 
-            Path(CKIP_DIR).mkdir(parents=True, exist_ok=True)
+            Path(config.ckip_dir).mkdir(parents=True, exist_ok=True)
             drivers = (
-                CkipWordSegmenter(model=self._nlp_model),
-                CkipPosTagger(model=self._nlp_model),
+                CkipWordSegmenter(model=config.nlp_model),
+                CkipPosTagger(model=config.nlp_model),
             )
 
-            with open(rf"{self._ckip_path}", "wb") as file:
+            with open(rf"{config.config_drivers_path}", "wb") as file:
                 pickle.dump(drivers, file)
 
     def connect(self) -> tuple:
@@ -49,5 +39,6 @@ class CKIPClient:
         Returns:
             a tuple that contains CkipWordSegmenter and CkipPosTagger.
         """
-        with open(self._ckip_path, "rb") as file:
+
+        with open(config.config_drivers_path, "rb") as file:
             return pickle.load(file)
